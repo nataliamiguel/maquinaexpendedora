177cdc2f483313db8b6b52e320d747bf2119e403
Natalia <natalia.miguel@alumnos.upm.es>
2023-12-12 17:38:01 +0100

    DISPLAY

---
+++ maquina_expend/maquina_expend.srcs/sources_1/new/DISPLAY_CH.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12.12.2023 17:37:35
+-- Design Name: 
+-- Module Name: DISPLAY_CH - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity DISPLAY_CH is
+--  Port ( );
+end DISPLAY_CH;
+
+architecture Behavioral of DISPLAY_CH is
+
+begin
+
+
+end Behavioral;
