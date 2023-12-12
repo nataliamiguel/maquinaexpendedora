e8b1c4329abbb4e12b29e9425c00497f5ffb6c9d
Natalia <natalia.miguel@alumnos.upm.es>
2023-12-12 17:25:13 +0100

    DISPLAY

---
+++ maquina_expend/maquina_expend.srcs/sources_1/new/DISPLAY_COUNTER.vhd
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12.12.2023 17:22:58
+-- Design Name: 
+-- Module Name: DISPLAY_COUNTER - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity DISPLAY_COUNTER is
+  Port ( clk : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           count_int : in STD_LOGIC;
+           count_dec : in STD_LOGIC;
+           digsel : out STD_LOGIC_VECTOR (3 downto 0);
+           segment : out STD_LOGIC_VECTOR (6 downto 0));
+           
+end DISPLAY_COUNTER;
+
+architecture Behavioral of DISPLAY_COUNTER is
+
+begin
+
+
+end Behavioral;
---
+++ maquina_expend/maquina_expend.srcs/sources_1/new/DISPLAY_ERR.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12.12.2023 17:22:58
+-- Design Name: 
+-- Module Name: DISPLAY_ERR - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity DISPLAY_ERR is
+   
+end DISPLAY_ERR;
+
+architecture Behavioral of DISPLAY_ERR is
+
+begin
+
+
+end Behavioral;
