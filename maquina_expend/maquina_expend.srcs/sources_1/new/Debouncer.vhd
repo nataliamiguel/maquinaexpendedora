b7c9ba3319cb34f111f504d1f8543bd3a8007ae0
tobiaskos <t.kosanich@alumnos.upm.es>
2023-12-15 12:19:14 +0100

    debouncer

---
+++ maquina_expend/maquina_expend.srcs/sources_1/new/Debouncer.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 15.12.2023 12:17:00
+-- Design Name: 
+-- Module Name: Debouncer - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Debouncer is
+    Port ( CLK : in STD_LOGIC;
+           COIN : in STD_LOGIC_VECTOR (3 downto 0);
+           COIN_OUT : in STD_LOGIC);
+end Debouncer;
+
+architecture Behavioral of Debouncer is
+
+begin
+
+
+end Behavioral;
