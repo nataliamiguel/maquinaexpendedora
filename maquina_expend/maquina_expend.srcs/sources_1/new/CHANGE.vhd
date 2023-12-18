39bd6625cba7454037389a645d4ab6a47c2c5aa9
Natalia <natalia.miguel@alumnos.upm.es>
2023-12-18 18:00:18 +0100

    CHANGE

--- maquina_expend/maquina_expend.srcs/sources_1/new/CHANGE.vhd
+++ maquina_expend/maquina_expend.srcs/sources_1/new/CHANGE.vhd
@@ -24,8 +24,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
@@ -34,7 +34,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity CHANGE is
     Port ( reset : in STD_LOGIC;
            clk : in STD_LOGIC;
-           option: in STD_LOGIC_vECTOR(1 downto 0);
+           option: in STD_LOGIC_vECTOR(2 downto 0); --100 agua; 010 coca; 001 cafe
            reassemble: in STD_LOGIC;
            count : in STD_LOGIC_VECTOR (6 downto 0);
            change: out STD_LOGIC_VECTOR (6 downto 0)
@@ -57,19 +57,34 @@ begin
         end if;
     end process;
 
-    return_change: process (clk, estado_actual)
+    return_change: process (clk, option)
     begin
-        if (option = "01") then --AGUA 1€
-            estado_actual<=S5;
-            --ME FALTA ACABAR
-        elsif (option="10") then --CAFE 0.7€
-            estado_actual<=S6;
-            
-        elsif (option="11") then --COCA 1.8€
-            estado_actual<=S7;
-            
-        else
-        
+        if (option = "100") then --AGUA 1€ 
+        --multiplicamos 1€ por 10 para quitar la parte decimal(si la hubiera)
+        --10 en binario es 1010 -> 0001010
+            estado_siguiente<=S5;
+            change<= (count-"0001010");
+        elsif (option="010") then  --COCA 1.8€
+        -- 18 en binario es 0010010
+            estado_siguiente<=S6;
+            change<= (count-"0010010");
+        elsif (option="001") then --CAFE 0.7€
+        -- 7 en binario es 0000111
+            estado_siguiente<=S7;
+            change<= (count-"0000111");
         end if;
+        estado_actual<=estado_siguiente;
     end process;
+    
+    reassemble_button: process (reassemble)
+    begin
+        --si pulsamos reassemble y estamos en los estados S5, S6 o S7, vuelve al estado S0
+        if (reassemble='1') then
+            if (estado_actual = S5 or estado_actual = S6 or estado_actual = S7) then
+            estado_siguiente<=S0;
+            end if;
+        end if;
+        estado_actual<=estado_siguiente;
+    end process;
+    
 end Behavioral;
